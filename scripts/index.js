99630f021728b0c36277bc2196fceec355d0207b
Matthew Freeman <matthew.freeman821@gmail.com>
2018-06-01 14:53:49 -0400

    Updated text content in center and reset button, refactored reset code

--- scripts/index.js
+++ scripts/index.js
@@ -8,47 +8,37 @@ var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector('#message');
 var h1 = document.querySelector('h1');
 var resetButton = document.querySelector('#reset');
-var easyBtn = document.querySelector('#easyBtn');
-var hardBtn = document.querySelector('#hardBtn');
+var modeButtons = document.querySelectorAll('.mode');
 
-easyBtn.addEventListener('click', function() {
-    easyBtn.classList.add('selected');
-    hardBtn.classList.remove('selected');
-    numSquares = 3;
+for (var i = 0; i < modeButtons.length; i++) {
+    modeButtons[i].addEventListener('click', function() {
+        modeButtons[0].classList.remove('selected');
+        modeButtons[1].classList.remove('selected');
+        this.classList.add('selected');
+        this.textContent === 'Easy' ? numSquares = 3: numSquares = 6;
+        reset();
+    });
+}
+
+function reset() {
     colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
+    messageDisplay.textContent = '';
+    resetButton.textContent = 'New Colors';
     for(var i = 0; i < squares.length; i++) {
         if(colors[i]) {
+            squares[i].style.display = 'block';
             squares[i].style.backgroundColor = colors[i];
         } else {
             squares[i].style.display = 'none';
         }
     }
-})
-
-hardBtn.addEventListener('click', function() {
-    hardBtn.classList.add('selected');
-    easyBtn.classList.remove('selected');
-    numSquares = 6;
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    for(var i = 0; i < squares.length; i++) {
-        squares[i].style.backgroundColor = colors[i];
-        squares[i].style.display = 'block';
-        }
-    })
-
+    h1.style.backgroundColor = 'steelblue';
+}
 
 resetButton.addEventListener('click', function() {
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    for(var i = 0; i < squares.length; i++) {
-        squares[i].style.backgroundColor = colors[i];
-    }
-    h1.style.backgroundColor = 'steelblue';
+    reset();
 })
 
 colorDisplay.textContent = pickedColor;
